2a5105691c7dae67f8d7fa290ab14e023d0ecb2d
Alberto Montes <alm59321@gmail.com>
2016-10-27 08:56:39 +0200

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,12 @@
+# ZSH and Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp .zsh_alias ~/
 cp .zshrc ~/
-mkdir ~/.tmux
+cp terminal/gitster.zsh-theme ~/.oh-my-zsh/custom/themes/
+
+# TMUX
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .tmux.conf ~/
 
 # Atom
