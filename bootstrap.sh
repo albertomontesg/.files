43203639175cb6b339408d8a8882b532acce2a78
Alberto Montes <alm59321@gmail.com>
2016-10-31 19:00:09 +0100

    Added new files to the repo and change the script to link

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,24 @@
+CURRENT_DIR=$(pwd)
+
 # ZSH and Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp .zsh_alias ~/
-cp .zshrc ~/
-cp terminal/gitster.zsh-theme ~/.oh-my-zsh/custom/themes/
+ln -s $CURRENT_DIR/terminal/.zsh_alias ~/.zsh_alias
+ln -s $CURRENT_DIR/terminal/.zshrc ~/.zshrc
+ln -s $CURRENT_DIR/terminal/theme/gitster.zsh-theme ~/.oh-my-zsh/custom/themes/
 
 # TMUX
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp .tmux.conf ~/
+ln -s ~/.files/.tmux.conf ~/.tmux.conf
 
 # Atom
-cp atom/config.cson ~/.atom/
+for file in $CURRENT_DIR/atom/*{!.list} ; do
+    ln -s $CURRENT_DIR/atom/$file ~/.atom/$file
+done
 apm i --packages-file atom/packages.list
+
+# Git
+ln -s $CURRENT_DIR/git/.gitconfig ~/.gitconfig
+ln -s $CURRENT_DIR/git/.gitignore ~/.gitignore
+
+# Brew and Cask
