fc36efd3953981fcc04b4b772c01066137d4ecaf
Alberto Montes <alm59321@gmail.com>
2016-10-31 19:03:21 +0100

    Remove the dot at all the files

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,14 @@ CURRENT_DIR=$(pwd)
 
 # ZSH and Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -s $CURRENT_DIR/terminal/.zsh_alias ~/.zsh_alias
-ln -s $CURRENT_DIR/terminal/.zshrc ~/.zshrc
+ln -s $CURRENT_DIR/terminal/zsh_alias ~/.zsh_alias
+ln -s $CURRENT_DIR/terminal/zshrc ~/.zshrc
 ln -s $CURRENT_DIR/terminal/theme/gitster.zsh-theme ~/.oh-my-zsh/custom/themes/
 
 # TMUX
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s ~/.files/.tmux.conf ~/.tmux.conf
+ln -s ~/.files/tmux.conf ~/.tmux.conf
 
 # Atom
 for file in $CURRENT_DIR/atom/*{!.list} ; do
@@ -18,7 +18,7 @@ done
 apm i --packages-file atom/packages.list
 
 # Git
-ln -s $CURRENT_DIR/git/.gitconfig ~/.gitconfig
-ln -s $CURRENT_DIR/git/.gitignore ~/.gitignore
+ln -s $CURRENT_DIR/git/gitconfig ~/.gitconfig
+ln -s $CURRENT_DIR/git/gitignore ~/.gitignore
 
 # Brew and Cask
