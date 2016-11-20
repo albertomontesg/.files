8f62d578ef2664c0c1b5223cad9374b5495d7b33
Alberto Montes <alm59321@gmail.com>
2016-11-20 11:34:02 +0100

    Add functions source file and little modifications

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,28 @@
 CURRENT_DIR=$(pwd)
 
-# # ZSH and Oh My Zsh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# ln -s $CURRENT_DIR/terminal/zsh_alias ~/.zsh_alias
-# ln -s $CURRENT_DIR/terminal/zshrc ~/.zshrc
-# ln -s $CURRENT_DIR/terminal/theme/gitster.zsh-theme ~/.oh-my-zsh/custom/themes/
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-#
-# # TMUX
-# mkdir -p ~/.tmux/plugins
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# ln -s ~/.files/tmux.conf ~/.tmux.conf
-#
-# # Atom
-# for file in $CURRENT_DIR/atom ; do
-#     ln -s $CURRENT_DIR/atom/$file ~/.atom/$file
-# done
-# apm i --packages-file atom/packages.list
-#
-# # Git
-# ln -s $CURRENT_DIR/git/gitconfig ~/.gitconfig
-# ln -s $CURRENT_DIR/git/gitignore ~/.gitignore
+# ZSH and Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -s $CURRENT_DIR/terminal/zsh_alias ~/.zsh_alias
+ln -s $CURRENT_DIR/terminal/zsh_functions ~/.zsh_functions
+ln -s $CURRENT_DIR/terminal/zshrc ~/.zshrc
+ln -s $CURRENT_DIR/terminal/theme/gitster.zsh-theme ~/.oh-my-zsh/custom/themes/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# TMUX
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s ~/.files/tmux.conf ~/.tmux.conf
+
+# Atom
+for file in $CURRENT_DIR/atom ; do
+    ln -s $CURRENT_DIR/atom/$file ~/.atom/$file
+done
+apm i --packages-file atom/packages.list
+
+# Git
+ln -s $CURRENT_DIR/git/gitconfig ~/.gitconfig
+ln -s $CURRENT_DIR/git/gitignore ~/.gitignore
 
 # Binaries
 for file in $CURRENT_DIR/bin/* ; do
